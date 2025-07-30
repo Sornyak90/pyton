b4451740b352ef65a6bb996cfc3f11c909ad6d29
Sornyak90 <pomatveretin@gmail.com>
2025-07-30 13:45:34 -0400

    commit ghost gobble

--- exercism/python/currency-exchange/exchange.py
+++ exercism/python/currency-exchange/exchange.py
@@ -15,7 +15,7 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
 
-    pass
+    return budget/exchange_rate 
 
 
 def get_change(budget, exchanging_value):
@@ -26,7 +26,8 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
+
 
 
 def get_value_of_bills(denomination, number_of_bills):
@@ -37,7 +38,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - calculated value of the bills.
     """
 
-    pass
+    return denomination * number_of_bills
 
 
 def get_number_of_bills(amount, denomination):
@@ -48,7 +49,7 @@ def get_number_of_bills(amount, denomination):
     :return: int - number of bills that can be obtained from the amount.
     """
 
-    pass
+    return int(amount / denomination)
 
 
 def get_leftover_of_bills(amount, denomination):
@@ -59,7 +60,7 @@ def get_leftover_of_bills(amount, denomination):
     :return: float - the amount that is "leftover", given the current denomination.
     """
 
-    pass
+    return amount - int(amount/denomination) * denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -71,5 +72,7 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - maximum value you can get.
     """
-
-    pass
+    procent = exchange_rate + exchange_rate * (spread / 100)
+    a = budget / procent
+    count = int(a / denomination) 
+    return denomination * count
--- exercism/python/guidos-gorgeous-lasagna/lasagna.py
+++ exercism/python/guidos-gorgeous-lasagna/lasagna.py
@@ -7,26 +7,28 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
-#TODO: define your EXPECTED_BAKE_TIME (required) and PREPARATION_TIME (optional) constants below.
+
 EXPECTED_BAKE_TIME = 40
 
 
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(real_time):
+    """Calculate the elapsed cooking time.
+
+    :param number_of_layers: int - the number of layers in the lasagna.
+    """
     return EXPECTED_BAKE_TIME - real_time
     
 
 
 def preparation_time_in_minutes(number_of_layers):
+    """Calculate the elapsed cooking time.
+
+    :param number_of_layers: int - the number of layers in the lasagna.
+    """
     return number_of_layers * 2
 
 
-
-
-#TODO: define the 'elapsed_time_in_minutes()' function below.
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
-    prep_time = number_of_layers * 2
-    return prep_time + elapsed_bake_time
     """Calculate the elapsed cooking time.
 
     :param number_of_layers: int - the number of layers in the lasagna.
@@ -37,8 +39,5 @@ def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     time already spent baking and calculates the total elapsed minutes spent cooking the
     lasagna.
     """
-
-
-# TODO: Remember to go back and add docstrings to all your functions
-#  (you can copy and then alter the one from bake_time_remaining.)
-# def 
+    prep_time = number_of_layers * 2
+    return prep_time + elapsed_bake_time
