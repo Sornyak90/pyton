ecaf7fab5e2c927f2793ac5b88618c0750f66e97
Sornyak90 <pomatveretin@gmail.com>
2025-07-29 23:49:21 -0400

    update

--- exercism/python/guidos-gorgeous-lasagna/lasagna.py
+++ exercism/python/guidos-gorgeous-lasagna/lasagna.py
@@ -12,18 +12,8 @@ EXPECTED_BAKE_TIME = 40
 
 
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
-    """Calculate the bake time remaining.
-
-    :param elapsed_bake_time: int - baking time already elapsed.
-    :return: int - remaining bake time (in minutes) derived from  
-    'EXPECTED_BAKE_TIME'.
-
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-    pass
+def bake_time_remaining(real_time):
+    return EXPECTED_BAKE_TIME - real_time
 
 
 def preparation_time_in_minutes():
