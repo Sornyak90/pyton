83408b6ef7cc6f534b9d35e21e6180ded3c8c77b
Sornyak90 <pomatveretin@gmail.com>
2025-08-03 13:50:17 +0800

    bob and rain

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,2 +1,15 @@
 def response(hey_bob):
-    pass
+    hey_bob_stripped = hey_bob.strip()  
+    is_shouting = hey_bob_stripped.isupper()  
+    is_question = hey_bob_stripped.endswith('?') if hey_bob_stripped else False  
+    
+    if not hey_bob_stripped:  
+        return "Fine. Be that way!"
+    elif is_shouting and is_question: 
+        return "Calm down, I know what I'm doing!"
+    elif is_shouting: 
+        return "Whoa, chill out!"
+    elif is_question: 
+        return "Sure."
+    else:  
+        return "Whatever."
