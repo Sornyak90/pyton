39a5381882dd45b2c75ff0a232ea3eadb79ecba9
Sornyak90 <pomatveretin@gmail.com>
2025-08-04 15:44:45 +0800

    isbn no_my_ver

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,28 +1,15 @@
 def is_valid(isbn):
-    res=False
-    sum_n=0
-    i=10
-    cleaned_text = ''.join(char for char in isbn if char.isalnum()) 
-    print(cleaned_text)
-    if len(cleaned_text) != 10:
-        res=False
-    elif any(c.isalpha() for c in cleaned_text[:-1]):
-        res=False
-    elif cleaned_text[-1] != 'X' and not cleaned_text.isdigit():
-        res=False
-    else:
-        if len(cleaned_text) == 10:
-            for num in cleaned_text:
-                if num != 'X':
-                    sum_n+= int(num) * i
-                    i-=1
-                else:
-                    sum_n+= 10 * i
-                    i-=1
+    cleaned = ''.join(c for c in isbn if c.isalnum())
+    if len(cleaned) != 10:
+        return False
+    if any(c.isalpha() for c in cleaned[:-1]):
+        return False
+    if cleaned[-1] not in '0123456789X':
+        return False
     
-    if sum_n % 11 == 0 and sum_n != 0:
-        res = True
-           
-    return res
-
-print(is_valid("3598215088"))
+    total = 0
+    for i, char in enumerate(cleaned):
+        value = 10 if char == 'X' else int(char)
+        total += value * (10 - i)
+    
+    return total % 11 == 0 and total != 0
