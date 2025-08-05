4e34a53db57dc858fcdd6832625c1518a898fd2c
Sornyak90 <pomatveretin@gmail.com>
2025-08-05 09:51:41 +0800

    darts full

---
+++ exercism/python/darts/darts.py
@@ -0,0 +1,24 @@
+def score(x, y):
+    x0, y0 = 0, 0  
+    r1 = 1
+    r2 = 5
+    r3 = 10
+    res=0
+
+    dist = ((x)**2 + (y)**2)**0.5
+
+    if dist <= 1:
+        res = 10
+    elif 1 < dist <= 5:
+        res = 5
+    elif 5 < dist <= 10:
+        res = 1
+    else:
+        res = 0
+    return res
+
+
+    
+
+
+print(score(-0.1, -0.1))
