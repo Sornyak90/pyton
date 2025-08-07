d64061dc92a0b9469e3db7bd90236b2635635c3b
Sornyak90 <pomatveretin@gmail.com>
2025-08-07 15:47:13 +0800

    square_newton_full

---
+++ exercism/python/square-root/square_root.py
@@ -0,0 +1,13 @@
+DELTA = 0.0001
+
+def square_root(number):
+    x0 =  number/2
+    x2 = 1
+    while x2 > DELTA:
+        x1 = round((x0 + (number/x0))/2,3)
+        x2= round(abs(x1 -x0),3)
+        x0=x1
+        
+    return int(x1)
+             
+print(square_root(25))  
