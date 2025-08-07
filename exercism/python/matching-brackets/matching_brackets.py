c68233640e7760091c3924b28bcf8a7cdacbe0ef
Sornyak90 <pomatveretin@gmail.com>
2025-08-07 21:27:33 +0800

    ({[

--- exercism/python/matching-brackets/matching_brackets.py
+++ exercism/python/matching-brackets/matching_brackets.py
@@ -1,2 +1,24 @@
 def is_paired(input_string):
-    pass
+    skobki = '(){}[]'
+    skobki_l = '({['
+    input_string = [inx for inx in input_string if inx in skobki]
+    result = True
+    stek = []
+    for inx in input_string:
+        if inx in skobki_l:
+            stek.append(inx)
+        else:
+            if len(stek) != 0 and inx == ')' and stek[-1] == '(':
+                stek.pop()
+            elif len(stek) != 0 and inx == '}' and stek[-1] == '{':
+                stek.pop()
+            elif len(stek) != 0 and inx  == ']' and stek[-1] == '[':
+                stek.pop()
+            else:
+                result = False
+                break
+    else:
+        if len(stek) != 0:
+            result =False
+
+    return result
