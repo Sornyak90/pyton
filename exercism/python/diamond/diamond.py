7952f699c75bb31f23b985c74affc401c6716f8f
Sornyak90 <pomatveretin@gmail.com>
2025-08-08 15:33:02 +0800

    diamond II

--- exercism/python/diamond/diamond.py
+++ exercism/python/diamond/diamond.py
@@ -1,19 +1,11 @@
 def rows(letter):
-    inx = 2 * (ord(letter) - 65) + 1
-    cent = inx // 2
-    n_l = cent
-    n_r = cent
+    size = 2 * (ord(letter) - 65) + 1
+    center = size // 2
     result = []
-
-    for i in range(cent+1):
-        list1 = list(" " * inx)
-        list1[n_l] = chr(65 + i)
-        list1[n_r] = chr(65 + i)
-        n_l-=1
-        n_r+=1
-        str1 = "".join(i for i in list1)
-        result.append(str1)
-    for i in result[-2::-1]:
-        result.append(i)
-
-    return result
+    
+    for i in range(center + 1):
+        row = [' '] * size
+        row[center - i] = row[center + i] = chr(65 + i)
+        result.append(''.join(row))
+    
+    return result + result[-2::-1]
