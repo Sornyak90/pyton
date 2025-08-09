5cd06efa7c439d132ec9f71682927a1a77520922
Sornyak90 <pomatveretin@gmail.com>
2025-08-09 13:59:35 +0800

    secret full II

--- exercism/python/secret-handshake/secret_handshake.py
+++ exercism/python/secret-handshake/secret_handshake.py
@@ -1,18 +1,3 @@
 def commands(binary_str):
-    code_word = [
-        " ",
-        "jump",
-        "close your eyes",
-        "double blink",
-        "wink"]
-    result_list = [code_word[i] for i in range(1,len(binary_str)) if int(binary_str[i])]
-    if int(binary_str[0]) == 0: 
-        result_list.reverse()
-    return result_list
-
-
-
-
-print(commands("00011"))    
-    
-
+    code_word = ["", "jump", "close your eyes", "double blink", "wink"]
+    return [w for i, w in enumerate(code_word[1:], 1) if int(binary_str[i])][::1 if int(binary_str[0]) else -1]
