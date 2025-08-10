ab58155d46c68a891998fcc382d4390da366f6c3
Sornyak90 <pomatveretin@gmail.com>
2025-08-10 22:02:07 +0800

    eggs full

---
+++ exercism/python/eliuds-eggs/eliuds_eggs.py
@@ -0,0 +1,12 @@
+def egg_count(display_value):
+    decimal = 0
+    binary = []
+    # dic -> bite
+    while display_value >= 2:
+        remains = display_value % 2
+        decimal = int(display_value / 2)
+        binary.insert(0, remains)
+        display_value = decimal
+    else:
+        binary.insert(0, decimal)
+    return binary.count(1)cd
