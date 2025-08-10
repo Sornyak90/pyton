00c2c0db77bbfbc739f71abbbf6e8e2c4879eac1
Sornyak90 <pomatveretin@gmail.com>
2025-08-10 21:42:00 +0800

    full search

--- exercism/python/binary-search/binary_search.py
+++ exercism/python/binary-search/binary_search.py
@@ -1,25 +1,14 @@
 def find(search_list, value):
-    i = len(search_list) // 2 
     _l = 0
-    _r = len(search_list)
-    if value not in search_list:
-        raise ValueError("value not in array")        
-        
+    _r = len(search_list) - 1  # правая граница включительно
 
-   
-    while search_list[i] != value and  _l < _r:
-        if value < search_list[i]:
-            _r = i
-            i = (_r - _l) // 2
+    while _l <= _r:
+        i = _l + (_r - _l) // 2
+        if search_list[i] == value:
+            return i
+        elif value < search_list[i]:
+            _r = i - 1
         else:
-            _l = i
-            i = _l + (_r - _l) // 2
-            
-    
-           
+            _l = i + 1
 
-            
-    return i
-
-        
-        
+    raise ValueError("value not in array")
