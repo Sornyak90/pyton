a51d15d1d6d8bff5c0b64bd2c15232cb4d4150b7
Sornyak90 <pomatveretin@gmail.com>
2025-08-13 13:25:05 +0800

    prime_factory full

--- exercism/python/prime-factors/prime_factors.py
+++ exercism/python/prime-factors/prime_factors.py
@@ -1,2 +1,15 @@
 def factors(value):
-    pass
+    res =[]
+    if value > 1:
+        i = 2
+        while value != 1:
+            if value % i == 0:
+                value//=i
+                res.append(i)
+                i = 2
+            else:
+                i+=1
+    return res
+
+print(factors(4))
+
