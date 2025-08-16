21334f055c5f464c36c4ac4f938da72340830020
Sornyak90 <pomatveretin@gmail.com>
2025-08-16 23:06:23 +0800

    etl full

--- exercism/python/dominoes/dominoes.py
+++ exercism/python/dominoes/dominoes.py
@@ -1,38 +1,78 @@
+from collections import defaultdict, deque
+
+
+def dfs(graph, start, path):
+    stack = [start]
+    while stack:
+        current_vertex = stack[-1]
+        if graph[current_vertex]:
+            next_vertex = graph[current_vertex].pop()
+            stack.append(next_vertex)
+        else:
+            path.append(stack.pop())
+
+
+def find_eulerian_path_or_cycle(edges):
+    degrees = defaultdict(int)
+    graph = defaultdict(list)
+    
+    # Построение графа и подсчёт степеней вершин
+    for u, v in edges:
+        graph[u].append(v)
+        degrees[u] += 1
+        degrees[v] -= 1
+        
+    # Поиск стартовой вершины
+    odd_degree_vertices = [v for v, d in degrees.items() if abs(d) > 0]
+    if len(odd_degree_vertices) > 2:
+        return None
+    elif len(odd_degree_vertices) == 2:
+        start_vertex = odd_degree_vertices[0]
+    else:
+        start_vertex = min(degrees.keys())  # Можно начать с любого узла, если цикл замкнутый
+    
+    # DFS для нахождения Эйлерова пути/цикла
+    path = []
+    dfs(graph, start_vertex, path)
+    
+    # Возвращаем обратный порядок пути
+    return path[::-1]
+
+
 def can_chain(dominoes):
-    result = None
-    
-    if dominoes == []:
+    if not dominoes:
         return []
-
-    domino_dict = {}
-    for row in dominoes:
-        for key in row:
-            domino_dict[key] = domino_dict.get(key, 0) + 1
-
-    for value in domino_dict.values():
-        if value % 2 != 0:
-            return result
     
-    count_num = len(domino_dict)
-    total_sum = sum(domino_dict.values())
-    quotient = total_sum//count_num
-
-    if abs(quotient - count_num) == 1:
-        result_domino = [dominoes[0]]
-        for rows in dominoes[1:]:
-           if result_domino[-1][1] in rows:
-            if result_domino[-1][1] == rows[0]:
-                result_domino.append(rows)
-            else:
-                result_domino.append(reversed(rows))
-
-           
+    # Преобразование костяшек домино в рёбра графа
+    edges = [(u, v) for u, v in dominoes]
+    
+    eulerian_path = find_eulerian_path_or_cycle(edges)
+    if eulerian_path is None:
+        return None
+    
+    # Формируем итоговую последовательность домино
+    result = []
+    prev_vertex = None
+    for vertex in eulerian_path[:-1]:  # Исключаем последнюю вершину
+        if prev_vertex is not None:
+            matching_domino = next((d for d in dominoes if sorted([prev_vertex, vertex]) == sorted(d)), None)
+            if matching_domino is None:
+                return None
             
-           
+            # Ориентируем костяшку правильно
+            if matching_domino[0] == prev_vertex:
+                result.append(matching_domino)
+            else:
+                result.append((matching_domino[1], matching_domino[0]))
+        
+        prev_vertex = vertex
     
-
     return result
 
-# input_dominoes = [(1, 2), (2, 3), (3, 1), (4, 5), (5, 6), (6, 4)]
-input_dominoes = [(1, 2), (3, 1), (2, 3)]
-print(can_chain(input_dominoes))
+
+# Тестовые случаи
+if __name__ == "__main__":
+    print(can_chain([]))                      # Должно вернуть пустой список
+    print(can_chain([(1, 1)]))               # Должно вернуть [(1, 1)]
+    print(can_chain([(1, 2)]))               # Нет правильного решения
+    print(can_chain([(1, 2), (3, 1), (2, 3)])) # Должно вернуть одну из возможных правильных последовательностей
--- exercism/python/dominoes/dominoes_test.py
+++ exercism/python/dominoes/dominoes_test.py
@@ -10,10 +10,10 @@ from dominoes import (
 
 
 class DominoesTest(unittest.TestCase):
-    # def test_empty_input_empty_output(self):
-    #     input_dominoes = []
-    #     output_chain = can_chain(input_dominoes)
-    #     self.assert_correct_chain(input_dominoes, output_chain)
+    def test_empty_input_empty_output(self):
+        input_dominoes = []
+        output_chain = can_chain(input_dominoes)
+        self.assert_correct_chain(input_dominoes, output_chain)
 
     # def test_singleton_input_singleton_output(self):
     #     input_dominoes = [(1, 1)]
@@ -25,10 +25,10 @@ class DominoesTest(unittest.TestCase):
     #     output_chain = can_chain(input_dominoes)
     #     self.refute_correct_chain(input_dominoes, output_chain)
 
-    def test_three_elements(self):
-        input_dominoes = [(1, 2), (3, 1), (2, 3)]
-        output_chain = can_chain(input_dominoes)
-        self.assert_correct_chain(input_dominoes, output_chain)
+    # def test_three_elements(self):
+    #     input_dominoes = [(1, 2), (3, 1), (2, 3)]
+    #     output_chain = can_chain(input_dominoes)
+    #     self.assert_correct_chain(input_dominoes, output_chain)
 
     # def test_can_reverse_dominoes(self):
     #     input_dominoes = [(1, 2), (1, 3), (2, 3)]
@@ -85,48 +85,48 @@ class DominoesTest(unittest.TestCase):
     #     output_chain = can_chain(input_dominoes)
     #     self.refute_correct_chain(input_dominoes, output_chain)
 
-    # # Utility methods
+    # # # Utility methods
 
-    def normalize_dominoes(self, dominoes):
-        return list(sorted(tuple(sorted(domino)) for domino in dominoes))
+    # def normalize_dominoes(self, dominoes):
+    #     return list(sorted(tuple(sorted(domino)) for domino in dominoes))
 
-    def assert_same_dominoes(self, input_dominoes, output_chain):
-        msg = (
-            "Dominoes used in the output must be the same "
-            "as the ones given in the input"
-        )
-        input_normal = self.normalize_dominoes(input_dominoes)
-        output_normal = self.normalize_dominoes(output_chain)
-        self.assertEqual(input_normal, output_normal, msg)
+    # def assert_same_dominoes(self, input_dominoes, output_chain):
+    #     msg = (
+    #         "Dominoes used in the output must be the same "
+    #         "as the ones given in the input"
+    #     )
+    #     input_normal = self.normalize_dominoes(input_dominoes)
+    #     output_normal = self.normalize_dominoes(output_chain)
+    #     self.assertEqual(input_normal, output_normal, msg)
 
-    def assert_consecutive_dominoes_match(self, output_chain):
-        for i in range(len(output_chain) - 1):
-            msg = (
-                "In chain {}, right end of domino {} ({}) "
-                "and left end of domino {} ({}) must match"
-            )
-            msg = msg.format(
-                output_chain, i, output_chain[i], i + 1, output_chain[i + 1]
-            )
-            self.assertEqual(output_chain[i][1], output_chain[i + 1][0], msg)
+    # def assert_consecutive_dominoes_match(self, output_chain):
+    #     for i in range(len(output_chain) - 1):
+    #         msg = (
+    #             "In chain {}, right end of domino {} ({}) "
+    #             "and left end of domino {} ({}) must match"
+    #         )
+    #         msg = msg.format(
+    #             output_chain, i, output_chain[i], i + 1, output_chain[i + 1]
+    #         )
+    #         self.assertEqual(output_chain[i][1], output_chain[i + 1][0], msg)
 
-    def assert_dominoes_at_ends_match(self, output_chain):
-        msg = (
-            "In chain {}, left end of first domino ({}) and "
-            "right end of last domino ({}) must match"
-        )
-        msg = msg.format(output_chain, output_chain[0], output_chain[-1])
-        self.assertEqual(output_chain[0][0], output_chain[-1][1], msg)
+    # def assert_dominoes_at_ends_match(self, output_chain):
+    #     msg = (
+    #         "In chain {}, left end of first domino ({}) and "
+    #         "right end of last domino ({}) must match"
+    #     )
+    #     msg = msg.format(output_chain, output_chain[0], output_chain[-1])
+    #     self.assertEqual(output_chain[0][0], output_chain[-1][1], msg)
 
-    def assert_correct_chain(self, input_dominoes, output_chain):
-        msg = "There should be a chain for {}".format(input_dominoes)
-        self.assertIsNotNone(output_chain, msg)
-        self.assert_same_dominoes(input_dominoes, output_chain)
-        if not any(output_chain):
-            return
-        self.assert_consecutive_dominoes_match(output_chain)
-        self.assert_dominoes_at_ends_match(output_chain)
+    # def assert_correct_chain(self, input_dominoes, output_chain):
+    #     msg = "There should be a chain for {}".format(input_dominoes)
+    #     self.assertIsNotNone(output_chain, msg)
+    #     self.assert_same_dominoes(input_dominoes, output_chain)
+    #     if not any(output_chain):
+    #         return
+    #     self.assert_consecutive_dominoes_match(output_chain)
+    #     self.assert_dominoes_at_ends_match(output_chain)
 
-    def refute_correct_chain(self, input_dominoes, output_chain):
-        msg = "There should be no valid chain for {}".format(input_dominoes)
-        self.assertIsNone(output_chain, msg)
+    # def refute_correct_chain(self, input_dominoes, output_chain):
+    #     msg = "There should be no valid chain for {}".format(input_dominoes)
+    #     self.assertIsNone(output_chain, msg)
