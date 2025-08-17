15b515b3248b689e39781eb64ce319a2934e0ded
Sornyak90 <pomatveretin@gmail.com>
2025-08-17 13:14:01 +0800

    space-age full

---
+++ exercism/python/space-age/space_age.py
@@ -0,0 +1,31 @@
+class SpaceAge:
+    YEAR = 31557600
+
+    def __init__(self, seconds):
+        self.seconds = seconds
+    
+    def on_earth(self):
+        return round(self.seconds/(self.YEAR * 1.0), 2)
+
+    def on_mercury(self):
+        return round(self.seconds/(self.YEAR * 0.2408467), 2)
+        
+
+    def on_venus(self):
+        return round(self.seconds/(self.YEAR * 0.61519726), 2)
+
+    def on_mars(self):
+        return round(self.seconds/(self.YEAR * 1.8808158), 2)
+
+    def on_jupiter(self):
+        return round(self.seconds/(self.YEAR * 11.862615), 2)
+
+    def on_saturn(self):
+        return round(self.seconds/(self.YEAR * 29.447498), 2)
+
+    def on_uranus(self):
+        return round(self.seconds/(self.YEAR * 84.016846), 2)
+
+    def on_neptune(self):
+        return round(self.seconds/(self.YEAR * 164.79132), 2)
+        
