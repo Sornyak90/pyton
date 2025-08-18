5774fcdfd6eb91f737fe61d64dea9aeecc17a16f
Sornyak90 <pomatveretin@gmail.com>
2025-08-18 23:40:02 +0900

    cater full

---
+++ exercism/python/cater-waiter/sets.py
@@ -0,0 +1,71 @@
+"""Functions for compiling dishes and ingredients for a catering company."""
+
+
+from sets_categories_data import (VEGAN,
+                                  VEGETARIAN,
+                                  KETO,
+                                  PALEO,
+                                  OMNIVORE,
+                                  ALCOHOLS,
+                                  SPECIAL_INGREDIENTS)
+
+
+def clean_ingredients(dish_name, dish_ingredients):
+    return (dish_name, set(dish_ingredients))
+
+
+def check_drinks(drink_name, drink_ingredients):
+    for drink in drink_ingredients:
+        if drink in ALCOHOLS:
+            return drink_name + " Cocktail"
+        
+    return  drink_name + " Mocktail"
+    
+
+
+def categorize_dish(dish_name, dish_ingredients):
+    if dish_ingredients.issubset(VEGAN):
+        return f"{dish_name}: VEGAN"
+    elif dish_ingredients.issubset(VEGETARIAN):
+        return f"{dish_name}: VEGETARIAN"
+    elif dish_ingredients.issubset(KETO):
+        return f"{dish_name}: KETO"
+    elif dish_ingredients.issubset(PALEO):
+        return f"{dish_name}: PALEO"
+    elif dish_ingredients.issubset(OMNIVORE):
+        return f"{dish_name}: OMNIVORE"
+    return None
+
+
+def tag_special_ingredients(dish):
+    name, ingredient = dish
+    ingtid_alirg = set()
+    for product in ingredient:
+        if product in SPECIAL_INGREDIENTS:
+            ingtid_alirg.add(product)
+    return (name, set(sorted(list(ingtid_alirg))))
+
+def compile_ingredients(dishes):
+    list_ingr =set()
+    for dishe in dishes:
+        for ingr in dishe:
+            list_ingr.add(ingr)
+
+    return list_ingr
+    
+
+
+def separate_appetizers(dishes, appetizers):
+    res = set()
+    for dishe in dishes:
+        if dishe not in appetizers:
+            res.add(dishe)
+    return list(res)
+
+
+def singleton_ingredients(dishes, intersection):
+    all_ingr = set().union(*dishes)
+    res = all_ingr - intersection
+    return res
+
+print(tag_special_ingredients(('Ginger Glazed Tofu Cutlets', ['tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'])))
